21d7e71aa07ad9ee725376aa83b5bd4469c8ac4f
wov1a <vova.stoan2006@gmail.com>
2023-06-04 01:43:04 +0300

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:netschool/responsive/mobailSreenLayout.dart';
 import 'package:netschool/responsive/responsiveLayout.dart';
 import 'package:netschool/responsive/webScreenLayout.dart';
 import 'package:netschool/utils/colors.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+      apiKey: 'AIzaSyBfvyoyVLo916rSvocV6oqUg_6nSlx8CE0',
+      appId: "1:443250163052:web:cbf8cec4dcd3aefb44c749",
+      messagingSenderId: "443250163052",
+      projectId: "netschool-a296d",
+      storageBucket: "netschool-a296d.appspot.com",
+    ));
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
