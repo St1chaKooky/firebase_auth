72e85afab6e282f5a756c8c0b88f016aabf1ff7a
wov1a <vova.stoan2006@gmail.com>
2023-06-12 18:32:30 +0300

    Register Auth, not photo

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -1,15 +1,50 @@
-// import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:typed_data';
 
-// class AuthMethods {
-//   final FirebaseAuth _auth = FirebaseAuth.instance;
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:netschool/resources/storage_methods.dart';
 
-//   //sing up user
-//   Future <String> siginUpUser({
-//     required String email,
-//     required String password,
-//     required String username,
-//     required String bio,
-//   })async {
+class AuthMethods {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  //sing up user
+  Future<String> siginUpUser({
+    required String email,
+    required String password,
+    required String username,
+    required String bio,
+    // required Uint8List file,
+  }) async {
+    String res = "Some error occured";
+    try {
+      if (email.isNotEmpty ||
+          password.isNotEmpty ||
+          username.isNotEmpty ||
+          bio.isNotEmpty) {
+        //register user
+        UserCredential cred = await _auth.createUserWithEmailAndPassword(
+            email: email, password: password);
 
-//   }
-// }
+        print(cred.user!.uid);
+
+        // String photoUrl = await StorageMethods()
+        //     .uploadImageToStorage('profilePics', file, false);
+
+        //add user to our database
+        await _firestore.collection('users').doc(cred.user!.uid).set({
+          'username': username,
+          'uid': cred.user!.uid,
+          'email': email,
+          'followers': [],
+          'following': [],
+          // 'photoUrl': photoUrl,
+        });
+        res = "succes";
+      }
+    } catch (err) {
+      res = err.toString();
+    }
+    return res;
+  }
+}
--- lib/screens/siginup_screen.dart
+++ lib/screens/siginup_screen.dart
@@ -1,6 +1,12 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:netschool/resources/auth_methods.dart';
 import 'package:netschool/utils/colors.dart';
 import 'package:netschool/widgets/text_field.dart';
+
+import '../utils/image_utils.dart';
 
 class SiginUpScreen extends StatefulWidget {
   const SiginUpScreen({super.key});
@@ -14,6 +20,8 @@ class _SiginUpScreenState extends State<SiginUpScreen> {
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _bioController = TextEditingController();
   final TextEditingController _userNameController = TextEditingController();
+  Uint8List? _image;
+  bool _isLoading = false;
 
   @override
   void dispose() {
@@ -22,6 +30,32 @@ class _SiginUpScreenState extends State<SiginUpScreen> {
     _passwordController.dispose();
     _bioController.dispose();
     _userNameController.dispose();
+  }
+
+  void selectImage() async {
+    Uint8List im = await pickImage(ImageSource.gallery);
+    setState(() {
+      _image = im;
+    });
+  }
+
+  void signUpUser() async {
+    setState(() {
+      _isLoading = true;
+    });
+    String res = await AuthMethods().siginUpUser(
+      email: _emailController.text,
+      password: _passwordController.text,
+      username: _userNameController.text,
+      bio: _bioController.text,
+      // file: _image!,
+    );
+    setState(() {
+      _isLoading = false;
+    });
+    if (res != 'succes') {
+      showSnakBar(res, context);
+    } else {}
   }
 
   @override
@@ -76,7 +110,7 @@ class _SiginUpScreenState extends State<SiginUpScreen> {
                 height: 12,
               ),
               InkWell(
-                onTap: () => print('1'),
+                onTap: signUpUser,
                 child: Container(
                     width: double.infinity,
                     alignment: Alignment.center,
@@ -89,8 +123,14 @@ class _SiginUpScreenState extends State<SiginUpScreen> {
                           ),
                         ),
                         color: blueColor),
-                    child: const Text('Log in',
-                        style: TextStyle(color: Colors.white))),
+                    child: _isLoading
+                        ? const Center(
+                            child: CircularProgressIndicator(
+                              color: whiteColor,
+                            ),
+                          )
+                        : const Text('Sign Up',
+                            style: TextStyle(color: Colors.white))),
               ),
               Flexible(
                 flex: 2,
