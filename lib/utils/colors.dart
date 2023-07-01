787ae22e9186a4c74d3aea3e3f1416e960419ce8
wov1a <vova.stoan2006@gmail.com>
2023-07-01 15:36:30 +0300

    blue заменил

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 
 const whiteColor = Color.fromRGBO(255, 255, 255, 1);
 const mobileSearchColor = Color.fromRGBO(124, 124, 124, 1);
-const blueColor = Color.fromRGBO(25, 0, 255, 1);
+// const blueColor = Color.fromRGBO(19, 99, 223, 1);
+const blueColor = Color.fromRGBO(35, 13, 231, 1);
+// const blueColor = Color.fromRGBO(255, 123, 41, 1);
+
 const secondaryColor = Colors.grey;
 const blackColor = Colors.black;
 const fieldColor = Color.fromARGB(255, 245, 244, 244);
